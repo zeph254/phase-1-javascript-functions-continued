917daf363322655d541d213282f2d8343016eaac
zephania owuor <ularezephaniah@gmail.com>
2024-10-01 16:16:45 +0300

    functions-continued

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity= "roller-skate ") {
+    console.log(`This Saturday, I want to ${activity}`);
+    
+}
+
+function mondayWork(work="go to the office") {
+    console.log(`This Monday, I will ${work}`);
+
+}
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+        return "You are " + flair + adjective + flair + "!";
+    };
+}
